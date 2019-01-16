5cc511cbdd5029ca6f1cfc89a694e5f6ffdaef9a
Olivier <Olivier.julien2@usherbrooke.ca>
2019-01-16 11:08:23 -0500

    Add second LED on pin 12

--- TestCodeArduino/TestCodeArduino.ino
+++ TestCodeArduino/TestCodeArduino.ino
@@ -18,12 +18,15 @@
 void setup() {
   // initialize digital pin 13 as an output.
   pinMode(13, OUTPUT);
+  pinMode(12, OUTPUT);
 }
 
 // the loop function runs over and over again forever
 void loop() {
+  digitalWrite(12, LOW);
   digitalWrite(13, HIGH);   // turn the LED on (HIGH is the voltage level)
   delay(1000);              // wait for a second
+  digitalWrite(12, HIGH);
   digitalWrite(13, LOW);    // turn the LED off by making the voltage LOW
   delay(1000);              // wait for a second
 }
