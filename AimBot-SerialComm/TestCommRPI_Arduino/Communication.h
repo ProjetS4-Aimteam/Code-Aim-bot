93094ad25ca55ada1ed4d8b8507578e112daa241
Carl-Philippe <37631656+Carl-Philippe@users.noreply.github.com>
2019-02-23 18:34:06 -0500

    Add files via upload
    Add SerialComm file from Oli

---
+++ AimBot-SerialComm/TestCommRPI_Arduino/Communication.h
@@ -0,0 +1,72 @@
+#ifndef Comm
+#define Comm
+
+#include "SimpleTimer.h"    // Librairie de chronomètres
+#include "ArduinoJson.h"    // Librairie de syntaxe JSON
+
+//========================================================================================================
+//========================================================================================================
+//======================Variables communication===========================================================
+
+// Contrôle du temps
+bool    should_send_ ;        // Indique qu'on est prêt à transmettre l'état.
+SimpleTimer timer;                  // objet du chronomètre
+ 
+
+// Message objects
+StaticJsonBuffer<500> jsonBuffer;
+JsonVariant pi_msg = jsonBuffer.createObject();
+JsonObject& state_msg = jsonBuffer.createObject();
+
+
+
+//========================================================================================================
+//========================================================================================================
+//======================Fonction de communication=========================================================
+void timerCallback(){
+  /* Rappel du chronometre */
+  should_send_ = true;          // Indique qu'on est prêt à mettre à jour et transmettre
+}
+
+void analyseMessage(JsonVariant msg){
+  /* Fonction d'analyse du message recu */
+  JsonVariant parse_msg;
+  // Determiner si le message est un succes
+  if (!msg.success()) {
+    // Condition d'erreur : On ignore.
+    return;
+  }
+ 
+  parse_msg =  msg["test"];
+  if(parse_msg.success()){
+    String buff_str = msg["test"];
+    if (buff_str.equals("77")){
+      cmdLedRed = true;
+    }
+    if (buff_str.equals("55")){
+      cmdLedRed = false;
+    }
+  }
+
+  
+
+  
+}
+
+
+void serialEvent(){
+  /* Rappel lors de reception sur port serie */
+  StaticJsonBuffer<500> jsonBuffer;
+  JsonObject& root = jsonBuffer.parseObject(Serial);
+  pi_msg = root;
+}
+
+
+void send_state_data(){
+  /* Fonction pour envoie de l'etat */
+  state_msg.printTo(Serial); // Met le message en attente dans le port serie
+  Serial.println();// Envoie le message
+}
+
+
+#endif
