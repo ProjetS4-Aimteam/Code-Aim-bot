31ec53aca74eaf0a47cbaca7d36ad0e9fe02bb91
Carl-Philippe <cyrc2702@usherbrooke.ca>
2019-04-04 11:41:11 -0400

    Bug catkin_make into the RQT_GUI

--- RQT_GUI/setup.py
+++ RQT_GUI/setup.py
@@ -4,6 +4,6 @@ from catkin_pkg.python_setup import generate_distutils_setup
 d = generate_distutils_setup(
 packages=['RQT_GUI'],
 package_dir={'': 'src'},
-scripts=['scripts/tracking_camera'],
+scripts=['scripts/RQT_GUI'],
 )
 setup(**d)
